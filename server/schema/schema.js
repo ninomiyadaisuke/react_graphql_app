fc2196bb189e17fb3e2dab32f45db560cac998e8
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-04 10:27:06 +0900

    create app.js schema.js connect to mongoDB

---
+++ server/schema/schema.js
@@ -0,0 +1,25 @@
+const graphql = require("graphql")
+
+const { GraphQLObjectType } = graphql 
+
+const MovieType = new GraphQLObjectType({
+    name: "Movie",
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString }
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        movie: {
+            type: MovieType,
+            args: { id: { type: GraphQLString } },
+            resolve(parents, args) {
+                
+            }
+        }
+    }
+})

fc9353e012df6230a43e5f689ae1262cc5237cb5
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-04 11:16:58 +0900

    get data from mongoDB

--- server/app.js
+++ server/app.js
@@ -1,15 +1,17 @@
 const express = require("express")
-const graphqlHTTP = require("express-graphql")
+const {graphqlHTTP} = require('express-graphql')
 const mongoose = require("mongoose")
+const schema = require("./schema/schema")
 const app = express()
 
 mongoose.connect("mongodb+srv://admin:test1234@cluster0.jx3ho.mongodb.net/test?retryWrites=true&w=majority")
 mongoose.connection.once("open", () => {
     console.log("db connected")
 })
-// app.use("/graphql", graphqlHTTP({
-
-// }))
+app.use("/graphql", graphqlHTTP({
+    schema,
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log("listening port 4000")
